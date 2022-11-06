e0e071eed11c82018f15cd4507fdb142d8b9732d
Jaden Bertino <jaden@bertinofamily.com>
2022-11-06 13:34:51 -0800

    Create javascript to close popup

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-// Initiate Pop Up
+/*
+
+Start Pop Up
+
+*/
+
 const popupStarters = [
   document.querySelector("#nav__about"),
   document.querySelector("#nav__contact"),
@@ -8,16 +13,40 @@ const popupStarters = [
 
 for (let btn of popupStarters) {
   const startPopUp = btn.addEventListener("click", () => {
-    document.querySelector(".popup").classList.add("popup--display")
+    // document.querySelector(".popup").classList.add("popup--opacity");
+    document.querySelector(".popup__wrapper").classList.add("popup__wrapper--visibility");
+    document.querySelector(".popup__wrapper").classList.add("popup__wrapper--opacity");
   })
 }
 
-// Close Pop Up
+/*
 
+Close Pop Up
+
+*/
+
+function closePopUp() {
+  const popup = document.querySelector(".popup__wrapper")
+  popup.classList.remove("popup__wrapper--opacity");
+  setTimeout(() => {
+    popup.classList.remove("popup__wrapper--visibility")
+  }, 500);
+}
+
+// Click X -> Close Pop Up
 const closeBtn = document.querySelector(".popup__exit-btn");
-closeBtn.addEventListener("click", () => {
-  document.querySelector(".popup").classList.remove("popup--display");
-});
+closeBtn.addEventListener("click", closePopUp);
+
+// Click outside of pop up -> close pop up
+const outsidePop = document.querySelector(".popup__wrapper");
+outsidePop.addEventListener("click", closePopUp);
+
+
+/* 
+
+CONTACT FORM
+
+*/
 
 // Submit Contact Form
 const contactForm = document.querySelector("#contact__form");
