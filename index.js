479286404fe9c3dc26375b096df8173e6740e0ac
Jaden Bertino <jaden@bertinofamily.com>
2022-11-06 11:11:43 -0800

    Create pop up starter-buttons

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// Initiate Pop Up
+const popupStarters = [
+  document.querySelector("#nav__about"),
+  document.querySelector("#nav__contact"),
+  document.querySelector("#social__btn--contact"),
+  document.querySelector("#footer__contact")
+];
+
+for (let btn of popupStarters) {
+  const startPopUp = btn.addEventListener("click", () => {
+    document.querySelector(".popup").classList.add("popup--display")
+  })
+}
+
+// Submit Contact Form
 const contactForm = document.querySelector("#contact__form");
 
 contactForm.addEventListener("submit", (event) => {
